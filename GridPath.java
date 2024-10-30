865cd0921f844042187c9a421395647350edcdb9
michaelz126 <1zhang.michael@gmail.com>
2024-10-30 13:32:54 +0000

    update gridpath

--- GridPath.java
+++ GridPath.java
@@ -2,9 +2,40 @@ public class GridPath {
     /** Initialized in the constructor with distinct values that never change */
     private int[][] grid;
 
-    public GridPath(int[][])
-
-
+    public GridPath(int[][] values)
+    {
+        grid = values;
+    }
+/* 
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row ++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s+- grid[row][col] + " ";
+            } 
+            s+= "\n";
+        }
+        return s;
+    }
+*/
+    public String toString() {
+    String s = "";
+        for (int[] row : grid) 
+        {
+            for (int col : row)
+            {
+            s += col + " ";
+            }
+            s += "\n";
+        }
+        return s;   
+        }
+    
+        
+        
     /**
      * Returns the Location representing a neighbor of the grid element at row and
      * col,
@@ -14,8 +45,27 @@ public class GridPath {
      * row and col do not specify the element in the last row and last column of
      * grid.
      */
-    public Location getNextLoc(int row, int col) {
-        /* to be implemented in part (a) */ }
+    public Location getNextLoc(int row, int col) 
+    {
+        int below = Integer.MAX_VALUE;
+        if (row < grid.length - 1) below = grid[row + 1][col];
+        int right = Integer.MAX_VALUE;
+        if(col < grid[0].length - 1) right = grid[row][col + 1];
+    }
+        
+        
+        
+        
+        
+        
+        
+        /* 
+        int below = grid[row + 1][col];
+        int right = grid[row][col + 1];
+        if(below < right) return new Location(row + 1, col);
+        else return new Location(row,col + 1);
+    }
+    
 
     /**
      * Computes and returns the sum of all values on a path through grid, as
@@ -27,5 +77,6 @@ public class GridPath {
      * grid.
      */
     public int sumPath(int row, int col) {
-        /* to be implemented in part (b) */ }
+        return 0;
+    }
 }
