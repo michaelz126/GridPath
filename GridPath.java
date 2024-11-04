d9af4503ad30f0e44969b9aed93fa4159213ae11
michaelz126 <1zhang.michael@gmail.com>
2024-11-04 14:32:27 +0000

    cleaned up comments

--- GridPath.java
+++ GridPath.java
@@ -1,26 +1,12 @@
 public class GridPath {
-    /** Initialized in the constructor with distinct values that never change */
+   
     private int[][] grid;
 
     public GridPath(int[][] values)
     {
         grid = values;
     }
-/* 
-    public String toString()
-    {
-        String s = "";
-        for (int row = 0; row < grid.length; row ++)
-        {
-            for (int col = 0; col < grid[0].length; col++)
-            {
-                s+- grid[row][col] + " ";
-            } 
-            s+= "\n";
-        }
-        return s;
-    }
-*/
+
     public String toString() {
     String s = "";
         for (int[] row : grid) 
@@ -34,17 +20,6 @@ public class GridPath {
         return s;   
         }
     
-        
-        
-    /**
-     * Returns the Location representing a neighbor of the grid element at row and
-     * col,
-     * as described in part (a)
-     * Preconditions: row is a valid row index and col is a valid column index in
-     * grid.
-     * row and col do not specify the element in the last row and last column of
-     * grid.
-     */
     public Location getNextLoc(int row, int col) 
     {
         int below = Integer.MAX_VALUE;
@@ -57,26 +32,7 @@ public class GridPath {
         
         
         
-        
-        
-        
-        
-        /* 
-        int below = grid[row + 1][col];
-        int right = grid[row][col + 1];
-
-    }
     
-
-    /**
-     * Computes and returns the sum of all values on a path through grid, as
-     * described in
-     * part (b)
-     * Preconditions: row is a valid row index and col is a valid column index in
-     * grid.
-     * row and col do not specify the element in the last row and last column of
-     * grid.
-     */
     public int sumPath(int row, int col) {
         int sum = grid[row][col];
         while(row < grid.length -1 || col < grid[0].length - 1) {
